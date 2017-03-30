6d47e9be1a514e707aceea2b1403dca5d1c8f0c3
alvarosb <alvarosanchez.ciclos@gmail.com>
2017-03-30 12:09:24 +0200

    Prueba

---
+++ Prueba/src/Prueba.java
@@ -0,0 +1,14 @@
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+		// Array de string (4 palabras)
+		String [] palabras = {"diego","david","angel","jesus"};
+		for (int i=0; i<palabras.length; i++) { 
+			System.out.print(palabras[i]+"\t");
+		}
+	
+	}
+}
